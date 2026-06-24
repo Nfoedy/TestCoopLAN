09ac0b3724fa6e6b9489e5b73bc94e070b5442e9
RiccardoConforti <riccardoconforti96@gmail.com>
2026-06-24 15:15:28 +0200

    Initial Unreal project setup

---
+++ Source/TestCoopLAN/TestCoopLAN.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestCoopLAN.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestCoopLAN, "TestCoopLAN" );
+
+DEFINE_LOG_CATEGORY(LogTestCoopLAN)
---
+++ Source/TestCoopLAN/TestCoopLANGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestCoopLANGameMode.h"
+
+ATestCoopLANGameMode::ATestCoopLANGameMode()
+{
+	// stub
+}
