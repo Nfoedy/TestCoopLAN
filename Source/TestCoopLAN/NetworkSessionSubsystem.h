bad9cfa8ffc1f47891ae0a8e5164217fb094d025
RiccardoConforti <riccardoconforti96@gmail.com>
2026-07-03 14:40:02 +0200

    add NetworkSessionSubsystem files

---
+++ Source/TestCoopLAN/NetworkSessionSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "NetworkSessionSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCOOPLAN_API UNetworkSessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
